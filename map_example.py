f4f55651b540361eafeb6f3865c0daaf54ab4670
RamitaTamang <tamanngramita544@gmail.com>
2024-05-21 17:10:59 +0545

    map_example updated

--- map_example.py
+++ map_example.py
@@ -1,6 +1,16 @@
-def capitalize_and_ascii_sum(word: str):
-    return sum(ord(x) for x in word.capitalize())
 
-animals = ["ant","elephant","dog"]
-animal_output = list(map(capitalize_and_ascii_sum,animals))
-print(animal_output)
+
+#def capitalize_and_ascii_sum(word: str):
+ #   return sum(ord(x) for x in word.capitalize())
+
+#animals = ["ant","elephant","dog"]
+#animal_output = list(map(capitalize_and_ascii_sum,animals))
+#print(animal_output)
+
+def square(x):
+    return x**2
+    #lambda x: x**2
+numbers = [1,2,3,4]
+
+squares = map(lambda x: x**2, numbers)
+print(list(squares))
